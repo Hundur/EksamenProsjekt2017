2ccf124e9b804b8ce9681493c5aaaa03125980a9
kajahelenes <kaja.hurum@hotmail.com>
2017-05-11 10:11:15 +0200

    Update header.php, responsive navbar

--- header.php
+++ header.php
@@ -1,4 +1,28 @@
-<p id="overskrift">
-
-
-</p>
+<div class="nav">
+<div class="container">
+<div class="logo">
+<h1><img src="img/ww.png"></h1>
+	</div>
+<div class="toggle">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+      </div>
+	   
+	<div class="links">
+         <ul>
+            <li>
+               <a href="#">Forside</a>
+            </li>
+            <li>
+               <a href="#">Om oss</a>
+            </li>
+            <li>
+               <a href="#">Kontakt oss</a>
+            </li>
+         </ul>
+      </div>
+ <div class="clearfix"></div>
+   </div>
+</div>
