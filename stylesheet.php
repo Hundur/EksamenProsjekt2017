10e676c8e9250a43df4379ad4056f37eff09296b
kajahelenes <kaja.hurum@hotmail.com>
2017-05-11 10:15:07 +0200

    Updated on design

--- stylesheet.php
+++ stylesheet.php
@@ -3,18 +3,37 @@
 ?>
 
 
-html 
-{
-    background-image: url('img/vulkan.jpg');
+#container {
+    position: fixed;
+    overflow: hidden;
+    height: 100%;
+    width: 100%;
+    background-image:url('img/Vulkan1.jpg');
     background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-size: 100% 100%;
 }
 
-/* CSS main-navbar */
+/* CSS circle-navbar */
 
-.menutext
-{
+#heading {
+    position: absolute;
+    font-family: Raleway;
+    color: #f1f1f1;
+    top: 25%;
+    left: 36.5%;
+    font-size: 2.3em;
+    font-weight: 200;
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 4pt;
+}
+
+.menutext {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 300;
+    letter-spacing: 1.7pt;
+    font-family: 'Oswald', sans-serif; 
     float: left;
     width: 100%;
     padding-top: 25%;
@@ -23,176 +42,290 @@ html
     margin-top: -0.5em;
 }
 
-#menutext1 
-{
+#menutext1 {
     padding-top: 28%;
     line-height: 1.5em;
 }
 
-.menutext a 
-{
-    text-decoration: none;
-  color: white;
-  text-transform: uppercase;
-  font-weight: 300;
-  letter-spacing: 1.7pt;
-  font-family: 'Oswald', sans-serif; 
-}
-
-.menutext a:hover
-{
-    letter-spacing: 1.7pt;
-    font-weight: 400;
-    font-size: 1.2em;
-}
-
-.menucircle
-{
-    text-align: center;
+.menucircle {
     position: fixed;
-    height: 150px;
-    width: 150px;
+    text-align: center;
+    height: 135px;
+    width: 135px;
     border-radius: 200px;
-    background-color: #ff5252;
-    opacity: 0.8;
-    -webkit-box-shadow: 6px 10px 49px -9px #333;
-    -moz-box-shadow: 6px 10px 49px -9px #333;
-    box-shadow: 6px 10px 49px -9px #333;
+    border: 2px dotted white;
+    -webkit-transition: all 200ms linear;
+    -moz-transition: all 200ms linear;
+    -o-transition: all 200ms linear;
+    -ms-transition: all 200ms linear;
+    transition: all 200ms linear;
 }
 
-#food
-{
-    left: 21%;
-    top: 31%;
+/* For underpages */ 
+
+.menucircle2 { 
+ position: fixed;
+    text-align: center;
+    height: 100px;
+    width: 100px;
+    border-radius: 200px;
+    border: 2px dotted white;
+    -webkit-transition: all 200ms linear;
+    -moz-transition: all 200ms linear;
+    -o-transition: all 200ms linear;
+    -ms-transition: all 200ms linear;
+    transition: all 200ms linear;
+}
+@media screen and (max-width: 480px) {
+#heading {
+    left: 5%;
+
+}    
+.menucircle {
+        height: 60px;
+        width: 60px;
+        border: 2px solid transparent;
+    }
+}
+
+.menucircle2:hover {
+    font-weight: 500;
+    border: 2px solid transparent;
+}
+
+#menucircle21 {
+    left: 3%;
+    top: 12%;
     text-algin: center;
-    background-color: #7d5179;
+    background-color: #transparent;
 }
 
-#foodU
-{
-
+#menucircle22 {
+    left: 3%;
+    top: 30%;
 }
 
-#transport
-{
-    left: 32%;
-    top: 49.6%;
-    background-color: #6aafc8;
+#menucircle23 {
+    margin-left: 3%;
+    top: 48%;
 }
 
-#transportU
-{
-
+#menucircle24 {
+    left: 3%;
+    top: 59;
 }
 
-#shopping
-{
-    margin-left: 43%;
-    top: 32%;
+#menucircle25 {
+    left: 8%
+    bottom: 15%;
 }
 
-#shopping
-{
-
+#icons {
+    position: absolute;
 }
 
-#activities
-{
-    left: 54%;
-    top: 49.6%;
-    background-color: #ffd460;
+@media screen and (max-width: 480px) {
+#heading {
+    left: 5%;
+
+}    
+.menucircle2 {
+        height: 60px;
+        width: 60px;
+        border: 2px solid transparent;
+    }
 }
 
-#activities
-{
-
+.menucircle a {
+    background-color: red;
 }
 
-#arrangement
-{
-    left: 65%;
-    top: 31%;
-    background-color: #8ac394;
+.menucircle:hover {
+    font-weight: 500;
+    border: 2px solid transparent;
 }
 
-#icons 
-{
-    position: absolute;    
+#menucircle1 {
+    left: 22%;
+    top: 41%;
+    text-algin: center;
+    background-color: #transparent;
+}
+
+#menucircle2 {
+    left: 33%;
+    top: 59.6%;
+}
+
+#menucircle3 {
+    margin-left: 44.2%;
+    top: 41%;
+}
+
+#menucircle4 {
+    right: 33%;
+    top: 59.6%;
+}
+
+#menucircle5 {
+    right: 22%;
+    top: 41%;
+}
+
+#icons {
+    position: absolute;
 }
 
 
-/* CSS body style */
+/* Html, body */
 
 
-html, body
-{
+html, body {
     font-family: 'Helvetica Neue', sans-serif;
     margin: 0px;
     padding: 0px;
 }
 
 
-/* CSS Footer Menu */
-
-.footer
-{
-  position: absolute;
-  right: 0;
-  bottom: -300px;
-  width: 100%;
-  left: 0;
-  padding: 5rem;
-  background-color: #efefef;
-  text-align: center;
-}
-
-
-nav
-{
-	position: absolute;
-	bottom: 100px;
-	width: 100%;
-	height: 0px;
-	background: #6aafc8;
-    opacity: 0.8;
-}
-
-nav li 
-{
-	display: inline-block;
-	padding: 0px 35px;
-}
-
-nav li a
-{
-    font-family: 'Oswald', sans-serif;
-    font-size: 0.7em;
-    letter-spacing: 0.2pt;
+/* Navigation-bar */ 
+ 
+ html,
+ body,
+ h1,
+ ol,
+ ul {
+    margin: 0;
+    padding: 0;
+ }
+ 
+ ul,
+ ol {
+    list-style: none;
+ }
+ 
+ a {
     text-decoration: none;
-	color: #f1f1f1;
-	text-transform: uppercase;
+ }
+ 
+ .clearfix {
+    clear: both;
+ }
+
+ /* End Reset Some Elements */
+ 
+ .nav {
+    background-color: #fff;
+    opacity: 0.9;
+ }
+ 
+ .container {
+    width: 65%;
+    margin: 0 auto;
+ }
+
+ /* Start Toggle */
+ 
+  
+ .toggle {
+    width: 30px;
+    padding-top: 15px;
+    float: right;
+    margin: 6px;
+    cursor: pointer;
+    display: none;
+ } 
+ 
+ .toggle span {
+    width: 100%;
+    height: 4px;
+    background-color: #222;
+    display: block;
+    margin-bottom: 3px;
+ }
+
+ /* End Toggle */
+
+ /* Start Logo */
+ 
+
+ .nav .container .logo {
+    width: 25%;
+    float: left;
+    padding-top: 0px;
+    padding-bottom: 4px;
+ }
+ 
+
+ /* End Logo */
+
+ /* Start Links */
+ 
+ .nav .container .links {
+    width: 45%;
+    float: right;
+ }
+ 
+ .nav .container .links ul {
+    float: center;
+ }
+ 
+ .nav .container .links ul li {
+    display: inline-block;
+    padding: 20px;
+ }
+ 
+ .nav .container .links ul li a {
+    color: #333;
+    font-size: 0.7em;
+    font-family: Raleway;
+    text-transform: uppercase;
+    letter-spacing: 0.8pt;
+ }
+
+.nav .container .links ul li a:hover {
+   color: #f1f1f1;
+   padding: 10px 0 10px;
 }
+ /* End Links */
+
+ @media(max-width:768px) {
+    .toggle {
+       display: block;
+    }
+    .links {
+       display: none;
+    }
+    .nav .container .links ul li {
+       display: block;
+       text-align: center;
+       padding: 10px;
+    }
+    .nav .container .links ul {
+       float: none;
+    }
+    .nav .container .logo {
+       padding-top: 10px;
+    }
+ }
 
 
-/* For mobile, Ipad */
+<script>
 
-@media only screen and (max-width: 520px) {
+/* Javascript goes here */ 
 
-	nav li
-    {
-		padding: 24px 4px;
-	}
+$(document).ready(function() {
+    $(".toggle").click(function() {
+       $('.links').slideToggle();
+    });
+    $(window).resize(function() {
+       if ($(window).width() > 768) {
+          $('.links').show();
+       } else {
+          $('.links').hide();
+       }
+    });
+});
+	
+/* Javascript End */ 
 
-	nav li a 
-    {
-		font-size: 14px;
-	}
-
-}
-
-#logo 
-{ 
-   position: absolute; 
-   bottom: 40px; 
-   background-color: red;
-}
+	</script>
