3420d614a850504012622be5f73a92ef4519bdd0
kajahelenes <kaja.hurum@hotmail.com>
2017-05-11 10:10:22 +0200

    Updated index.php and header.php with responsive navbar

--- index.php
+++ index.php
@@ -1,32 +1,33 @@
 <!doctype html>
-<html>	
-    
+<html>
     <head>
-        <link rel = "stylesheet" type = "text/css" href = "stylesheet.php" />
-       <link href="https://fonts.googleapis.com/css?family=Oswald:200,400" rel="stylesheet">
+		<!-- Stylesheet link -->
+
+		<link rel = "stylesheet" type = "text/css" href = "style.php" /> 
+
+        <!-- Google font Import --> 
+
+       <link href="https://fonts.googleapis.com/css?family=Oswald:200,400,500" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500" rel="stylesheet">
-    </head>	
-
-    <body>
-        <div id="container">
+	</head>
+	<body>
+       
+		<div id="container">
 			
-            <?php 
-include_once('header.php');
-?>
-            <div class = menucircle id = food> <p class = "menutext" id = "txtfood"><img src="img/icons/food.png" alt="icons"><br><a href="matogdrikke.php">Mat & drikke</a></p> </div>
-			<div class = menucircle id = transport> <p class = "menutext" id = "txttransport"><img src="img/icons/transport.png" alt="icons"><br><a href="newside.php">Transport</a></p></div>
-			<div class = menucircle id = shopping> <p class = "menutext" id = "txtshopping"><img src="img/icons/shopping.png" alt="icons"><br><a href="newside.php">shop</a></p> 
-            </div>
-			<div class = menucircle id = activities> <p class = "menutext" id = "txtactivities"><img src="img/icons/activities.png" alt="icons"><br><a href="newside.php">Aktiviteter</a></p> </div>
-			<div class = menucircle id = arrangement> <p class = "menutext" id = "txtarrangement"><img src="img/icons/arrangement.png" alt="icons"><br><a href="newside.php">Arrangement</a></p> 
-			
-			</div>  <!-- End of main navbar -->
-
 
 			<?php 
-		include_once('footer.php');
-?>
-        </div>  
+
+include_once('header.php');
+?> 
+			<div id="heading">Vulkan <b>Guiden</b></div>
+			<a href="matogdrikke.php" class = menucircle id = menucircle1> <p class = "menutext" id = "menutext1"><img src="img/icons/food.png" alt="icons"><br>Food & drinks</p></a>
+			<a href="newside.php" class = menucircle id = menucircle2> <p class = "menutext" id = "menutext2"><img src="img/icons/transport.png" alt="icons"><br>Transport</p></a>
+			<a href="newside.php" class = menucircle id = menucircle3> <p class = "menutext" id = "menutext3"><img src="img/icons/shopping.png" alt="icons"><br>shop</p></a>
+			<a href="newside.php" class = menucircle id = menucircle4> <p class = "menutext" id = "menutext4"><img src="img/icons/activities.png" alt="icons"><br>Activities</p></a>
+			<a href="newside.php" class = menucircle id = menucircle5> <p class = "menutext" id = "menutext5"><img src="img/icons/arrangement.png" alt="icons"><br>Arrangements</p></a>  <!-- End of main navbar -->
+  
+		
+		</div>  <!-- End of container -->
     </body>
     
 </html>
