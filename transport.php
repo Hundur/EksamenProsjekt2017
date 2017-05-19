d0ebf6e815d2deb06508eb6ececfa36e2ca41b5d
kajahelenes <kaja.hurum@hotmail.com>
2017-05-19 12:23:50 +0200

    Update transport.php

--- transport.php
+++ transport.php
@@ -18,19 +18,18 @@
 include_once('header.php');
 ?> 
 			
-			<a href="food.php" class = menucircle2 id = food2> <p class = "menutext2" id = "menutext21"><img src="img/icons/food-27.png" alt="icons"><br>Food & drinks</p></a>
+		<a href="food.php" class = menucircle2 id = food2> <p class = "menutext2" id = "menutext21"><img src="img/icons/food-27.png" alt="icons"></p></a>
 			
-			<a href="transport.php" class = menucircle2 id = transport2> <p class = "menutext2" id = "menutext22"><img src="img/icons/transport-27.png" alt="icons"><br>Transport</p></a>
+			<a href="transport.php" class = menucircle2 id = transport2> <p class = "menutext2" id = "menutext22"><img src="img/icons/transport-27.png" alt="icons"></p></a>
 			
-			<a href="shop.php" class = menucircle2 id = shopping2> <p class = "menutext2" id = "menutext23"><img src="img/icons/shopping-27.png" alt="icons"><br>shop</p></a>
+			<a href="shop.php" class = menucircle2 id = shopping2> <p class = "menutext2" id = "menutext23"><img src="img/icons/shopping-27.png" alt="icons"></p></a>
 			
-			<a href="activities.php" class = menucircle2 id = activities2> <p class = "menutext2" id = "menutext24"><img src="img/icons/activities-27.png" alt="icons"><br>Activities</p></a>
-			
-			<a href="arrangements.php" class = menucircle2 id = arrangement2> <p class = "menutext2" id = "menutext25"><img src="img/icons/arrangement-27.png" alt="icons"><br>Arrangements</p></a> 
-			
+			<a href="activities.php" class = menucircle2 id = activities2> <p class = "menutext2" id = "menutext24"><img src="img/icons/activities-27.png" alt="icons"></p></a>
+
+		
 		<!-- End of main navbar -->
 		
-			<div id="container">
+			<div class="slide-container">
             
 		</div>  <!-- End of container -->
 		
