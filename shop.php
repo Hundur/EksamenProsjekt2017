84726f9ccb6ed17793420aa43721a7edc8c6b025
kajahelenes <kaja.hurum@hotmail.com>
2017-05-19 12:24:23 +0200

    Update shop.php

--- shop.php
+++ shop.php
@@ -18,52 +18,117 @@
 include_once('header.php');
 ?> 
 			
-			<a href="food.php" class = menucircle2 id = food2> <p class = "menutext2" id = "menutext21"><img src="img/icons/food-27.png" alt="icons"><br>Food & drinks</p></a>
+			<a href="food.php" class = menucircle2 id = food2> <p class = "menutext2" id = "menutext21"><img src="img/icons/food-27.png" alt="icons"></p></a>
 			
-			<a href="transport.php" class = menucircle2 id = transport2> <p class = "menutext2" id = "menutext22"><img src="img/icons/transport-27.png" alt="icons"><br>Transport</p></a>
+			<a href="transport.php" class = menucircle2 id = transport2> <p class = "menutext2" id = "menutext22"><img src="img/icons/transport-27.png" alt="icons"></p></a>
 			
-			<a href="shop.php" class = menucircle2 id = shopping2> <p class = "menutext2" id = "menutext23"><img src="img/icons/shopping-27.png" alt="icons"><br>shop</p></a>
+			<a href="shop.php" class = menucircle2 id = shopping2> <p class = "menutext2" id = "menutext23"><img src="img/icons/shopping-27.png" alt="icons"></p></a>
 			
-			<a href="activities.php" class = menucircle2 id = activities2> <p class = "menutext2" id = "menutext24"><img src="img/icons/activities-27.png" alt="icons"><br>Activities</p></a>
+			<a href="activities.php" class = menucircle2 id = activities2> <p class = "menutext2" id = "menutext24"><img src="img/icons/activities-27.png" alt="icons"></p></a>
 			
-			<a href="arrangements.php" class = menucircle2 id = arrangement2> <p class = "menutext2" id = "menutext25"><img src="img/icons/arrangement-27.png" alt="icons"><br>Arrangements</p></a> 
-			
-		<!-- End of main navbar -->
+		<!-- End of main menu -->
 		
-			<div id="container">
-            <div class="slide"><img class="Slides" src="img/Restaurants/dognvill.jpg" style="width:100%">
-                 <div class="textbox">
-					 <h4><b></b></h4>
-					 <p></p>
-                </div>
-            <img class="Slides" src="" style="width:100%">
-				<div class="textbox">
-					 <h4><b>Mathallen</b></h4>
-					 <p>Her skal det stå noe om mathallen</p>
-                </div>
-            <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-            <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
-			
-			
-		</div>  <!-- End of container -->
 		
-		    <script>
-        var slideIndex = 1;
-        showDivs(slideIndex);
-        function plusDivs(n) {
-        showDivs(slideIndex += n);
-        }
-        function showDivs(n) {
-        var i;
-        var x = document.getElementsByClassName("Slides");
-        if (n > x.length) {slideIndex = 1}    
-        if (n < 1) {slideIndex = x.length}
-        for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-        }
-        x[slideIndex-1].style.display = "block";  
-        }
+          <div class="slide-container">
+          <div class="Slide fade">
+          <img src="img/Shops/Vitusapotek.jpg" style="width:100%">
+	      <div class="textbox"><h3>Vitus Apotek</h3></div>
+             </div>
+			  
+		  <div class="Slide fade">
+          <img src="img/Restaurants/" style="width:100%">
+		  <div class="textbox"><h3>?</h3></div>
+              </div>
+
+          <div class="Slide fade">
+          <img src="img/Restaurants/" style="width:100%">
+          <div class="textbox"><h3>?</h3>
+			  <p></p></div>
+             </div>
+			  
+		  <div class="Slide fade">
+          <img src="img/Restaurants/" style="width:100%">
+          <div class="textbox"><h3>?</h3>
+			  <p></p></div>
+             </div>
+			  
+		  <div class="Slide fade">
+          <img src="img/Restaurants/" style="width:100%">
+          <div class="textbox"><h3>?</h3>
+			  <p></p></div>
+             </div>
+			  
+		  <div class="Slide fade">
+          <img src="img/Restaurants/" style="width:100%">
+          <div class="textbox"><h3>?</h3>
+			  <p></p></div>
+             </div>
+
+          <a class="back" onclick="plusSlides(-1)">&#10094;</a>
+          <a class="forward" onclick="plusSlides(1)">&#10095;</a>
+             </div>
+                   
+		      <br>
+
+          <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span> 
+            <span class="dot" onclick="currentSlide(2)"></span> 
+            <span class="dot" onclick="currentSlide(3)"></span> 
+                    
+		             </div>  <!-- End of slider -->
+		
+		   
+		
+		 <script>
+   
+		var slideIndex = 1;
+     
+	    showSlides(slideIndex);
+
+    
+				function plusSlides(n) {
+    
+					showSlides(slideIndex += n);
+				}
+
+				function currentSlide(n) {
+  
+					showSlides(slideIndex = n);
+   
+				}
+
+				function showSlides(n) {
+    
+					var i;
+    
+					var slides = document.getElementsByClassName("Slide");
+    
+					var dots = document.getElementsByClassName("dot");
+   
+					if (n > slides.length) {slideIndex = 1} 
+    
+					if (n < 1) {slideIndex = slides.length}
+    
+					for (i = 0; i < slides.length; i++) {
+     
+						slides[i].style.display = "none"; 
+    
+					}
+   
+		for (i = 0; i < dots.length; i++) {
+     
+			dots[i].className = dots[i].className.replace(" active", "");
+  
+		}
+ 
+		slides[slideIndex-1].style.display = "block"; 
+  
+		dots[slideIndex-1].className += " active";
+		
+			}
+       
     </script>
-		</div>
-    </body>
-</html>
+		
+		
+	     </body>
+       </html>
