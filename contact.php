5b3e4fc5954a69c2e10230b88ec7a51ef8b2ae1f
kajahelenes <kaja.hurum@hotmail.com>
2017-05-26 14:44:43 +0200

    Added files about.php, contact.php

---
+++ contact.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+    <head>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel = "stylesheet" type = "text/css" href = "style.php" /> 
+	  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+        <!-- Google font Import --> 
+
+       <link href="https://fonts.googleapis.com/css?family=Oswald:200,400,500" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500" rel="stylesheet">
+	</head>
+	<body>
+
+		
+		<?php 
+
+include_once('header.php');
+?> 
+			
+			<a href="food.php" class = menucircle2 id = food2> <p class = "menutext2" id = "menutext21"><img src="img/icons/food-27.png" alt="icons"></p></a>
+			
+			<a href="transport.php" class = menucircle2 id = transport2> <p class = "menutext2" id = "menutext22"><img src="img/icons/transport-27.png" alt="icons"></p></a>
+			
+			<a href="shop.php" class = menucircle2 id = shopping2> <p class = "menutext2" id = "menutext23"><img src="img/icons/shopping-27.png" alt="icons"></p></a>
+			
+			<a href="activities.php" class = menucircle2 id = activities2> <p class = "menutext2" id = "menutext24"><img src="img/icons/activities-27.png" alt="icons"></p></a>
+			
+		<!-- End of main navbar -->
+		
+			    <div class="slide-container">
+					<div class="about"><h2>Contact</h2><p>Har du et spørsmål, lurer på noe?</p><br><p>Send oss en mail!<br><br> westerdalsvulganguide@westerdals.no</p></div>
+				  
+      
+		</div>
+		
+	     </body>
+       </html>
