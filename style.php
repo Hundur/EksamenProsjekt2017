7660332edae9fd7157803b1243115fb5afa26f34
kajahelenes <kaja.hurum@hotmail.com>
2017-05-26 14:40:12 +0200

    Update style.php

--- style.php
+++ style.php
@@ -2,137 +2,7 @@
     header('Content-type: text/css; charset:UTF-8');
 ?>
 
-
-/* Font-family */
-
-html, 
-#heading,
-#title,
-h2, 
-h3,
-h4,
-p {
-  font-family: 'Raleway', sans-serif;
-} 
-
-/* Slider */ 
-
-* {box-sizing:border-box}
-
-
-/* Slideshow container */
-
-.slide-container {
-    position: fixed;
-    width: 64%;
-    top: 19%;
-    left: 18%;
-    height: 470px;
-    background: #f1f1f1;
-    border: 1px dashed #f1f1f1;
-    outline: 1px solid white;
-    outline-offset: 6px;
-    opacity: 1;
-    overflow: hidden;
-}
-  
-img {
-    opacity: 0.8;
-}
-
-.Slide {
-    display: none;
-}
-
-/* Forward and back buttons */
-
-.back, .forward {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  margin-top: -22px;
-  padding: 16px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-}
-
-.forward {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover effect */
-
-.back:hover, .forward:hover {
-  background-color: rgba(0,0,0,0.5);
-}
-
-/* Textbox */
-
-.textbox
-{
-    background: rgba(255,255,255,0.7);
-    position: absolute;
-    bottom: 10%;
-    left: 9%;
-    right: 9%;
-    height: 70%;
-    width: 80%;
-    padding: 40px;
-    overflow: hidden;
-}
-
-
-.dots {
-  cursor:pointer;
-  height: 12px;
-  width: 12px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-/* Slider End */ 
-
-p a {
-  font-family: 'Helvetica Neue', sans-serif;
-  font-size: 0.7em;
-  letter-spacing: -0.1pt;
-}
-
-h3, p { 
-  font-weight: 300;
-  text-align: center;
-}
+/* Background-image for all pages */
 
 html {
     background-image: url('img/Vulkan1.jpg');
@@ -141,11 +11,13 @@ html {
     background-size: 100% 100%;
 }
 
+/* Frontpage heading */ 
+
 #heading {
     position: absolute;
     color: #f1f1f1;
     top: 25%;
-    left: 32.5%;
+    left: 50%;
     font-size: 2.1em;
     font-weight: 200;
     text-align: center;
@@ -157,8 +29,7 @@ html {
 /* Circle-navbar for frontpage */
 
 
-/* For images inside circles */
-
+/* For icons inside circles */
 
 #icons {
     position: absolute;
@@ -177,7 +48,7 @@ html {
     float: left;
     width: 100%;
     padding-top: 26%;
-    font-size: 0.7em;
+    font-size: 0.8em;
     line-height: 2.9em;
     margin-top: -0.5em;
 }
@@ -203,9 +74,8 @@ html {
 }
 
 .menucircle:hover {
-    width: 175px;
-    height: 175px;
-    box-shadow: 0 20px 10px 0 rgba(0, 0, 0, 0.2), 0 10px 20px 0 rgba(0, 0, 0, 0.19);
+    width: 155px;
+    height: 155px;
 }
 
 #food {
@@ -215,10 +85,11 @@ html {
     background-color: #7d5179;
 }
 
+
 #transport {
     left: 33%;
     top: 59.6%;
-    background-color: #6aafc8;
+    background-color: #6aafc8; 
 }
 
 #shopping {
@@ -260,6 +131,147 @@ html {
     }
 }
 
+/* Slideshow */ 
+
+* {box-sizing:border-box}
+
+
+/* Slideshow container */
+
+.slide-container {
+    position: fixed;
+    width: 64%;
+    top: 19%;
+    left: 18%;
+    height: 470px;
+    background: #fff;
+    border: 1px dashed #fff;
+    outline: 1px solid white;
+    outline-offset: 6px;
+    opacity: 1;
+    overflow: hidden;
+}
+  
+img {
+    opacity: 0.8;
+}
+
+.Slide {
+    display: none;
+}
+
+/* Container for about, contact */
+
+
+/* Forward and back buttons */
+
+.back, .forward {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -22px;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+}
+
+.forward {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover effect */
+
+.back:hover, .forward:hover {
+  background-color: rgba(0,0,0,0.5);
+}
+
+/* Textbox for slider */
+
+.textbox
+{
+    background: rgba(255,255,255,0.7);
+    position: absolute;
+    bottom: 10%;
+    left: 9%;
+    right: 9%;
+    height: 70%;
+    width: 80%;
+    padding: 40px;
+    overflow: hidden;
+}
+
+
+/* Fading animation */
+
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* Slider End */ 
+
+
+/* Underpages headings, text */
+
+----
+
+  p {
+    font-family: 'Helvetica Neue', sans-serif;
+    font-size: 1em;
+    text-align: center;
+    
+    
+  }
+
+  p, p a {
+  font-family: 'Helvetica Neue', sans-serif;
+  font-size: 0.99em;
+  font-weight: 400;
+  text-align: center;
+  line-height: 24px;
+  color: #333;
+
+}
+
+/* Font-family */
+
+html, 
+#heading,
+#title,
+h1,
+h2,
+h3,
+h4,
+h5 {
+   font-family: 'Raleway', sans-serif;
+}
+
+
+h2, h3 {
+   text-align: center;
+   padding-bottom: 0pt;
+}
+
+color {
+color: #8ccde5; 
+}
 
 /* Circle-navbar for underpages */ 
 
@@ -466,7 +478,7 @@ html {
        display: none;
     }
     .nav .container .links ul li {
-       display: block;21
+       display: block;
        text-align: center;
        padding: 10px;
     }
@@ -478,119 +490,15 @@ html {
     }
  }
 
-#Kart {
+.about
+{
+    background: rgba(255,255,255,0.7);
     position: absolute;
-    width: 100%;
-    opacity: 1;
+    bottom: 10%;
+    left: 9%;
+    right: 9%;
+    height: 80%;
+    width: 80%;
+    padding: 40px;
+    overflow: hidden;
 }
-
-#OlafRyes {
-    width: 30px;
-    opacity: 1;
-    position: relative;
-    left: 510px;
-    top: -30px;
-}
-
-#OlafRyes:hover ~ #OR{
-    opacity: 1;
-}
-
-#OR {
-    outline: #d1c69a dotted;
-    position: relative;
-    background: #f7ebb7;
-    width: 130px;
-    height: 50px;
-    top: -70px;
-    left: 540px;
-    opacity: 1;
-}
-
-#Mollerveien {
-    width: 30px;
-    opacity: 1;
-    position: relative;
-    top: 350px;
-    left: 210px;
-}
-
-#Mollerveien:hover ~ #MV{
-    opacity: 1;
-}
-
-#MV {
-    outline: #d1c69a dotted;
-    position: relative;
-    background: #f7ebb7;
-    width: 110px;
-    height: 50px;
-    top: 310px;
-    left: 240px;
-    opacity: 1;
-}
-
-#Schousplass {
-    width: 30px;
-    opacity: 1;
-    position: relative;
-    top: 100px;
-    left: 515px;
-}
-
-#Schousplass:hover ~ #SP{
-    opacity: 1;
-}
-
-#SP {
-    outline: #d1c69a dotted;
-    position: relative;
-    background: #f7ebb7;
-    width: 110px;
-    height: 50px;
-    top: 60px;
-    left: 550px;
-    opacity: 1;
-}
- 
-#telthusbakken {
-    width: 30px;
-    opacity: 1;
-    position: relative;
-    top: -35px;
-    left: 175px;
-}
-
-#telthusbakken:hover ~ TB{
-    opacity: 1;
-}
-
-#TB{
-    outline: #d1c69a dotted;
-    position: relative;
-    background: #f7ebb7;
-    width: 110px;
-    height: 50px;
-    top: -80px;
-    left: 210px;
-    opacity: 1;
-}
-
-<script>
-/* Javascript goes here */ 
-
-$(document).ready(function() {
-    $(".toggle").click(function() {
-       $('.links').slideToggle();
-    });
-    $(window).resize(function() {
-       if ($(window).width() > 768) {
-          $('.links').show();
-       } else {
-          $('.links').hide();
-       }
-    });
-});
-	
-/* Javascript End */ 
-	</script>
